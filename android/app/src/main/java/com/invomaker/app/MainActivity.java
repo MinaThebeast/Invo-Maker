329608d321922b3e049f0c851b3a3d5be78f8661
Mina Arian <mano.mina.91@gmail.com>
2025-11-29 09:47:50 -0600

    Update bundle identifier to unique value: com.minaarian.invomaker

---
+++ android/app/src/main/java/com/invomaker/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.invomaker.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.invomaker.app',
+  appId: 'com.minaarian.invomaker',
   appName: 'INVO Maker',
   webDir: 'dist'
 };
